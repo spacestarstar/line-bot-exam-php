2c63400dca744e3024696e212d7bab2b9fdabb9a
spacestarstar <41611873+spacestarstar@users.noreply.github.com>
2018-07-24 14:02:09 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ghvyXfN0pTfg0DWfrgKOllrIHe6rhKEXg9MA26yrWgFvU9x362tMYPkRvdyv9MHA2LgCECjS8hvQiUyPDIfamrTfQiAt0yA2VdTUYRb44XA2stzfGj+z06khjgja+TQgQgGoUrYEyDuNWtAOVwGbIAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
